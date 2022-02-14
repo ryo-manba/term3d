9e45cbb77fbd43f50769087db73ab54a5502cde7
tkanzaki <tkanzaki@student.42tokyo.jp>
2022-02-14 16:18:38 +0100

    構造体名変更

--- includes/term3d.h
+++ includes/term3d.h
@@ -1,16 +1,16 @@
 #ifndef TERM3D_H
 # define TERM3D_H
 
-typedef struct s_transform
+typedef struct s_vector3
 {
 	long	x;
 	long	y;
 	long	z;
-}	t_transform;
+}	t_vector3;
 
 typedef struct s_vertex
 {
-	struct s_transform	*transform;
+	struct s_transform	*position;
 	struct s_vertex		*next;
 	struct s_vertex		*prev;
 }	t_vertex;
