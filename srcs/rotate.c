e82aa46deb523d9843a7087c6e46869f1077f479
tkanzaki <tkanzaki@student.42tokyo.jp>
2022-02-17 05:41:44 +0100

    一部修正6

--- srcs/rotate.c
+++ srcs/rotate.c
@@ -3,6 +3,7 @@
 static double get_vertical_element(const double magnitude, const double original_sin, const double original_cos, const double additional_radian);
 static double get_horizontal_element(const double magnitude, const double original_sin, const double original_cos, const double additional_radian);
 static double get_magnitude(const t_vector3 *position);
+static void rotate_one(t_vertex *index, const t_axis axis, const int degree);
 
 /**
  * 座標を反時計回りに軸回転させる
@@ -12,23 +13,35 @@ static double get_magnitude(const t_vector3 *position);
  */
 void	rotate(t_vertex *model_vertexes, const t_axis axis, const int degree)
 {
-	const double magnitude = get_magnitude(model_vertexes->position);
+	t_vertex *index;
+
+	index = model_vertexes;
+	while (index != NULL)
+	{
+		rotate_one(index, axis, degree);
+		index = index->next;
+	}
+}
+
+static void rotate_one(t_vertex *index, const t_axis axis, const int degree)
+{
+	const double magnitude = get_magnitude(index->position);
 	const double additional_radian = (((double)degree / 360) * 2 * M_PI);
 
 	if (axis == X_AXIS)
 	{
-		model_vertexes->position->y = get_vertical_element(magnitude, model_vertexes->position->y, model_vertexes->position->z, additional_radian);
-		model_vertexes->position->z = get_horizontal_element(magnitude, model_vertexes->position->y, model_vertexes->position->z, additional_radian);
+		index->position->y = get_vertical_element(magnitude, index->position->y, index->position->z, additional_radian);
+		index->position->z = get_horizontal_element(magnitude, index->position->y, index->position->z, additional_radian);
 	}
 	else if (axis == Y_AXIS)
 	{
-		model_vertexes->position->z = get_vertical_element(magnitude, model_vertexes->position->z, model_vertexes->position->x, additional_radian);
-		model_vertexes->position->x = get_horizontal_element(magnitude, model_vertexes->position->z, model_vertexes->position->x, additional_radian);
+		index->position->z = get_vertical_element(magnitude, index->position->z, index->position->x, additional_radian);
+		index->position->x = get_horizontal_element(magnitude, index->position->z, index->position->x, additional_radian);
 	}
 	else if (axis == Z_AXIS)
 	{
-		model_vertexes->position->y = get_vertical_element(magnitude, model_vertexes->position->y, model_vertexes->position->x, additional_radian);
-		model_vertexes->position->x = get_horizontal_element(magnitude, model_vertexes->position->y, model_vertexes->position->x, additional_radian);
+		index->position->y = get_vertical_element(magnitude, index->position->y, index->position->x, additional_radian);
+		index->position->x = get_horizontal_element(magnitude, index->position->y, index->position->x, additional_radian);
 	}
 	else
 	{
