a8424cd9b9f57c6fc6e65359a6f6a9c5d7d31ede
Ryomanba <datemanba3@gmail.com>
2022-02-17 13:32:32 +0900

    add: wrapper関数

---
+++ srcs/tm_wrapper.h
@@ -0,0 +1,19 @@
+#ifndef TM_WRAPPER_H
+# define TM_WRAPPER_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <ctype.h>
+#include <math.h>
+
+void *xmalloc(size_t size);
+FILE *xfopen(const char * restrict path, const char * restrict mode);
+void xfclose(FILE *stream);
+double	xstrtod(char *nptr);
+
+void perror_exit(void);
+
+#endif
