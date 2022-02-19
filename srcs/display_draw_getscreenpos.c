d5f39885ab34e7c76e04ebf42a899734d6f06bc2
tkanzaki <tkanzaki@student.42tokyo.jp>
2022-02-19 10:34:31 +0100

    視野角追加しカメラとの距離によって縮小

--- srcs/display_draw_getscreenpos.c
+++ srcs/display_draw_getscreenpos.c
@@ -1,6 +1,7 @@
 #include "display.h"
 
 static double get_perspective_pos(const t_axis axis, const double real_pos, const t_vertex *index, const t_camera *camera);
+static double distance_ratio(const t_camera *camera);
 
 double display_draw_getscreenpos(const t_axis axis, const double real_pos, const t_vertex *index, const t_camera *camera)
 {
@@ -16,8 +17,16 @@ double display_draw_getscreenpos(const t_axis axis, const double real_pos, const
 		// 透視投影
 		screen_pos = get_perspective_pos(axis, real_pos, index, camera);
 	}
+	screen_pos *= distance_ratio(camera);
 	screen_pos = floor(screen_pos);
 	return (screen_pos);
+}
+
+static double distance_ratio(const t_camera *camera)
+{
+	const double	view_width = fabs(camera->position->z) * sin(VIEW_ANGLE / 2) * 2;
+
+	return (DISPLAY_WIDTH / view_width);
 }
 
 static double get_perspective_pos(const t_axis axis, const double real_pos, const t_vertex *index, const t_camera *camera)
@@ -30,6 +39,6 @@ static double get_perspective_pos(const t_axis axis, const double real_pos, cons
 	{
 		tan_after = 0;
 	}
-	perspective_position = camera->position->z * (tan_before * (1 - tan_after * tan_after) / (1 - tan_after * tan_before) + tan_after);
+	perspective_position = camera->position->z * (tan_before * (1 - tan_after * tan_after) / (1 - tan_after * tan_before) - tan_after);
 	return (perspective_position);
 }
