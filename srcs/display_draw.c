b74923a05c3105f0d08e8f72a6e84c212e68f698
tkanzaki <tkanzaki@student.42tokyo.jp>
2022-02-19 09:15:40 +0100

    透視投影の回転ok?

--- srcs/display_draw.c
+++ srcs/display_draw.c
@@ -27,13 +27,15 @@ void	display_draw(char display[][DISPLAY_WIDTH], const t_vertex *model_vertexes,
 static int	get_screen_position(const t_axis axis, const t_vertex *index, const t_camera *camera)
 {
 	if (axis == X_AXIS)
-		return ((int)display_draw_getscreenpos(index->position->x,
+		return ((int)display_draw_getscreenpos(axis,
+												index->position->x,
 												index,
 												camera)
 												+ DISPLAY_WIDTH / 2
 												- camera->position->x);
 	else
-		return ((int)display_draw_getscreenpos(index->position->y,
+		return ((int)display_draw_getscreenpos(axis,
+												index->position->y,
 												index,
 												camera)
 												+ DISPLAY_HEIGHT / 2
--- srcs/display_draw_getscreenpos.c
+++ srcs/display_draw_getscreenpos.c
@@ -1,8 +1,8 @@
 #include "display.h"
 
-static double get_perspective_pos(const double real_pos, const t_vertex *index, const t_camera *camera);
+static double get_perspective_pos(const t_axis axis, const double real_pos, const t_vertex *index, const t_camera *camera);
 
-double display_draw_getscreenpos(const double real_pos, const t_vertex *index, const t_camera *camera)
+double display_draw_getscreenpos(const t_axis axis, const double real_pos, const t_vertex *index, const t_camera *camera)
 {
 	double		screen_pos;
 
@@ -14,18 +14,22 @@ double display_draw_getscreenpos(const double real_pos, const t_vertex *index, c
 	else
 	{
 		// 透視投影
-		screen_pos = get_perspective_pos(real_pos, index, camera);
+		screen_pos = get_perspective_pos(axis, real_pos, index, camera);
 	}
 	screen_pos = floor(screen_pos);
 	return (screen_pos);
 }
 
-static double get_perspective_pos(const double real_pos, const t_vertex *index, const t_camera *camera)
+static double get_perspective_pos(const t_axis axis, const double real_pos, const t_vertex *index, const t_camera *camera)
 {
-	double		distance_display;
-	double		distance_vertex;
-
-	distance_display = camera->position->z;
-	distance_vertex = distance_display - index->position->z;
-	return (real_pos * distance_display / distance_vertex);
+	double			perspective_position;
+	double			tan_before = real_pos / (camera->position->z - index->position->z);
+	double			tan_after =  tan((camera->horizontal_angle / 360) * 2 * M_PI);
+	
+	if (axis == Y_AXIS)
+	{
+		tan_after = 0;
+	}
+	perspective_position = camera->position->z * (tan_before * (1 - tan_after * tan_after) / (1 - tan_after * tan_before) + tan_after);
+	return (perspective_position);
 }
