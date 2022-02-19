bb3c121e8d3066d2eb796767f1c29e36bedd64c0
tkanzaki <tkanzaki@student.42tokyo.jp>
2022-02-19 15:24:41 +0100

    視野角外は画面外に点を描画する処理追加

--- srcs/display_draw.c
+++ srcs/display_draw.c
@@ -1,6 +1,7 @@
 #include "display.h"
 
 static int	get_screen_position(const t_axis axis, const t_vertex *index, const t_camera *camera);
+static bool	angle_check(const t_axis axis, const t_vertex *index, const t_camera *camera);
 
 /* 画面を描画 */
 void	display_draw(char display[][DISPLAY_WIDTH], const t_vertex *model_vertexes, const t_camera *camera)
@@ -26,6 +27,14 @@ void	display_draw(char display[][DISPLAY_WIDTH], const t_vertex *model_vertexes,
 
 static int	get_screen_position(const t_axis axis, const t_vertex *index, const t_camera *camera)
 {
+	bool	over_view;
+
+	// 視野角からはずれていたら画面の外に
+	over_view = angle_check(axis, index, camera);
+	if (over_view)
+	{
+		return (-1);
+	}
 	if (axis == X_AXIS)
 		return ((int)display_draw_getscreenpos(axis,
 												index->position->x,
@@ -40,4 +49,18 @@ static int	get_screen_position(const t_axis axis, const t_vertex *index, const t
 												camera)
 												+ DISPLAY_HEIGHT / 2
 												- camera->position->y);
+}
+
+static bool	angle_check(const t_axis axis, const t_vertex *index, const t_camera *camera)
+{
+	double angle;
+	if (axis == X_AXIS)
+	{
+		angle = atan(index->position->x / (index->position->z - camera->position->z)) * 180 / M_PI + camera->horizontal_angle;
+		if (angle > VIEW_ANGLE_WIDTH / 2)
+		{
+			return (true);
+		}
+	}
+	return (false);
 }
