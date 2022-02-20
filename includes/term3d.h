61a0a5cb27efb02ba24519588cd2f5768d76ff6e
tkanzaki <tkanzaki@student.42tokyo.jp>
2022-02-20 09:44:00 +0100

    平行投影修正中

--- includes/term3d.h
+++ includes/term3d.h
@@ -8,18 +8,18 @@
 # include <math.h>
 
 # define DISPLAY_HEIGHT 40
-# define DISPLAY_WIDTH 120
+# define DISPLAY_WIDTH 150
 # define VIEW_ANGLE_WIDTH 120 // 視野角
 # define VIEW_ANGLE_HEIGHT 80
 # define FRAMES_PER_SECOND 60 // FPS
-# define DEFAULT_CAMERA_MODE 0 // 0:平行投影 1:透視投影
+# define DEFAULT_CAMERA_MODE 1 // 0:平行投影 1:透視投影
 # define OBJ1_EXPANSION_RATE 3 // スケール
 # define OBJ2_EXPANSION_RATE 4
-# define OBJ1_ROTATE_SPEED_X 1 // 回転角度
-# define OBJ1_ROTATE_SPEED_Y 2
+# define OBJ1_ROTATE_SPEED_X 0 // 回転角度
+# define OBJ1_ROTATE_SPEED_Y 0
 # define OBJ1_ROTATE_SPEED_Z 0
-# define OBJ2_ROTATE_SPEED_X -1
-# define OBJ2_ROTATE_SPEED_Y 3
+# define OBJ2_ROTATE_SPEED_X 0
+# define OBJ2_ROTATE_SPEED_Y 0
 # define OBJ2_ROTATE_SPEED_Z 0
 # define OBJ1_PIVOT_X -15 // 回転ピボット
 # define OBJ1_PIVOT_Y -2
@@ -29,7 +29,7 @@
 # define OBJ2_PIVOT_Z 1
 # define CAMERA_POSITION_X 20 // 固定
 # define CAMERA_POSITION_Y -5
-# define CAMERA_POSITION_Z -140 // 固定
+# define CAMERA_POSITION_Z -80 // 固定
 # define CAMERA_ANGLE_HORIZONTAL 0 // 水平回転角度
 # define CAMERA_ANGLE_RANGE 60 // カメラ回転の可動域
 # define FILE_TYPE_THREED ','
